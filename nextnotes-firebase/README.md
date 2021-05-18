26c052c33c90466022b6c2277c29c4f9628c4dbf
Guillermo López García <lopezgarciaguillermo@live.com>
2021-05-18 20:13:43 +0200

    all endpoints work in local. Lack test in remote

--- nextnotes-firebase/README.md
+++ nextnotes-firebase/README.md
@@ -12,5 +12,32 @@ go run main.go
 
 ## To deploy in Google Cloud Platform Functions
 ```
+cd notes
 gcloud functions deploy Notes --runtime go113 --trigger-http --entry-point=Notes --memory=128MB
+```
+
+### Credentials to run in local
+```
+You need the file credentials.json with credentials of your project firebase.
+Example of file:
+{
+  "type": "service_account",
+  "project_id": "...",
+  "private_key_id": "...",
+  "private_key": "...",
+  "client_email": "...",
+  "client_id": "...",
+  "auth_uri": "...",
+  "token_uri": "...",
+  "auth_provider_x509_cert_url": "...",
+  "client_x509_cert_url": "..."
+}
+```
+
+## Test in local
+```
+code ext install humao.rest-client
+go run main.go
+code nextnotes-firebase/notes/note.http
+enjoy!
 ```
--- nextnotes-firebase/main.go
+++ nextnotes-firebase/main.go
@@ -1,72 +1,15 @@
-package notes
+package main
 
 import (
-	"context"
 	"fmt"
-	"html"
 	"log"
 	"net/http"
 
-	"cloud.google.com/go/firestore"
-	firebase "firebase.google.com/go"
+	"guilogar.com/nextnotes/notes"
 )
 
-type Note struct {
-	Name string
-}
-
-func getClientFirestore() (context.Context, *firestore.Client) {
-	ctx := context.Background()
-	config := &firebase.Config{ProjectID: firestore.DetectProjectID}
-	app, err := firebase.NewApp(ctx, config)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	client, err := app.Firestore(ctx)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	return ctx, client
-}
-
-func Notes(w http.ResponseWriter, r *http.Request) {
-	ctx, client := getClientFirestore()
-
-	switch r.Method {
-	case http.MethodGet:
-		name := r.URL.Query().Get("name")
-		fmt.Fprintf(w, "Hello, %s!", html.EscapeString(name))
-		ref, result, err := client.Collection("notes").Add(ctx, map[string]interface{}{
-			"first": name,
-			"last":  name,
-			"born":  1996,
-		})
-		_ = ref
-		_ = result
-
-		if err != nil {
-			log.Fatalf("Failed adding alovelace: %v", err)
-		}
-	case http.MethodPost:
-		http.Error(w, "403 - Forbidden", http.StatusForbidden)
-	case http.MethodPut:
-		http.Error(w, "403 - Forbidden", http.StatusForbidden)
-	case http.MethodDelete:
-		http.Error(w, "403 - Forbidden", http.StatusForbidden)
-	default:
-		http.Error(w, "405 - Method Not Allowed", http.StatusMethodNotAllowed)
-	}
-
-	// var d struct {
-	// 	Name string `json:"name"`
-	// }
-	// if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
-	// 	fmt.Fprint(w, "Hello, World!")
-	// 	return
-	// }
-	// if d.Name == "" {
-	// 	fmt.Fprint(w, "Hello, World!")
-	// 	return
-	// }
+func main() {
+	http.HandleFunc("/notes", notes.Notes)
+	fmt.Println("Backend init at localhost:5000")
+	log.Fatal(http.ListenAndServe(":5000", nil))
 }
