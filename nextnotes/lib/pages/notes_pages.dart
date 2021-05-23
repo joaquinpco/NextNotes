4cace372896279919d3f0fce2e9180656f6e4c9e
joaquinpco <joaquin1208@me.com>
2021-05-23 22:50:10 +0200

    Api getUri added with dotEnv and notes FutureBuilder Added

--- nextnotes/lib/pages/notes_pages.dart
+++ nextnotes/lib/pages/notes_pages.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:nextnotes/models/Note.dart';
+import 'package:nextnotes/utils/Api.dart';
 
 class NotesPage extends StatefulWidget {
   NotesPage({Key? key}) : super(key: key);
@@ -8,11 +11,24 @@ class NotesPage extends StatefulWidget {
 }
 
 class _NotesPageState extends State<NotesPage> {
+
+
+  Future<List<Note>> _retrieveAllNotesFromDatabase() async {
+    Uri url = Api().getUri("/notes");
+    dynamic response = await Api.httpGet(url);
+    print(response);
+    return response;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-     
-      
+    return FutureBuilder<List<Note>>(
+      future: _retrieveAllNotesFromDatabase(),
+      builder: (ctx, snapshot) {
+        List<Note>? notes = snapshot.data;
+        return Container();
+      },
     );
   }
+
 }
--- nextnotes/lib/utils/Api.dart
+++ nextnotes/lib/utils/Api.dart
@@ -1,6 +1,7 @@
 import 'dart:collection';
 
 import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
 
 class Api {
   static Future<http.Response?> httpGet(Uri url) async {
@@ -9,5 +10,17 @@ class Api {
 
   static Future<http.Response?> httpPost(Uri url, HashMap<String, String> body) async {
     return await http.post(url, body: body);
+  }
+
+  Uri getUri(String path, { Map<String, dynamic?>? params}) {
+    return params == null ? Uri.http(
+      "${DotEnv.env['HOST']}:${DotEnv.env['PORT']}", 
+      path
+    ) :
+    Uri.http(
+      "${DotEnv.env['HOST']}:${DotEnv.env['PORT']}", 
+      path,
+      params
+    );
   }
 }
